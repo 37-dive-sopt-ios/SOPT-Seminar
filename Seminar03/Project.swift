b3a1a29204ff307b007fe6eac3803a98cf5b8199
thingineeer <dlaudwls1203@gmail.com>
2025-10-28 17:13:37 +0900

    [Setting] 폴더 순서 세미나 순서대로 정렬하기

--- Seminar03/Project.swift
+++ Seminar03/Project.swift
@@ -1,10 +1,26 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let project = Project.framework(
+let project = Project(
     name: "Seminar03",
-    dependencies: [
-        .project(target: "Core", path: "../Core"),
-        .external(name: "SnapKit")
+    settings: .settings(configurations: allConfigurations),
+    targets: [
+        .target(
+            name: "Seminar03",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.th1ngjin.Seminar03",
+            infoPlist: .default,
+            sources: [
+                "Sources/MVC/**",
+                "Sources/TableViewSeminar/**",
+                "Sources/CollectionViewSeminar/**"
+            ],
+            resources: ["Resources/**"],
+            dependencies: [
+                .project(target: "Core", path: "../Core"),
+                .external(name: "SnapKit")
+            ]
+        )
     ]
 )
