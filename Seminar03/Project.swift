8040a0f5f7a150dbf10c93b667c6f68e64bcbd40
thingineeer <dlaudwls1203@gmail.com>
2025-10-28 17:25:48 +0900

    [Setting] 그룹명 수정

--- Seminar03/Project.swift
+++ Seminar03/Project.swift
@@ -1,26 +1,10 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let project = Project(
+let project = Project.framework(
     name: "Seminar03",
-    settings: .settings(configurations: allConfigurations),
-    targets: [
-        .target(
-            name: "Seminar03",
-            destinations: .iOS,
-            product: .framework,
-            bundleId: "com.th1ngjin.Seminar03",
-            infoPlist: .default,
-            sources: [
-                "Sources/MVC/**",
-                "Sources/TableViewSeminar/**",
-                "Sources/CollectionViewSeminar/**"
-            ],
-            resources: ["Resources/**"],
-            dependencies: [
-                .project(target: "Core", path: "../Core"),
-                .external(name: "SnapKit")
-            ]
-        )
+    dependencies: [
+        .project(target: "Core", path: "../Core"),
+        .external(name: "SnapKit")
     ]
 )
