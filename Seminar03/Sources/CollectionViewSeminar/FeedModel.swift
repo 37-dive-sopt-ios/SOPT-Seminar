37db0a0147b43ee82e71c8fb739764cc7920dc42
thingineeer <dlaudwls1203@gmail.com>
2025-10-28 16:59:36 +0900

    [Add] Feed Model 목데이터 4개 추가

--- Seminar03/Sources/CollectionViewSeminar/FeedModel.swift
+++ Seminar03/Sources/CollectionViewSeminar/FeedModel.swift
@@ -30,6 +30,10 @@ public struct FeedModel {
         FeedModel(itemImg: UIImage(named: "feed2") ?? UIImage(), name: "에어팟 프로", price: "300,000원", isScrap: false),
         FeedModel(itemImg: UIImage(named: "feed3") ?? UIImage(), name: "커피머신", price: "100,000원", isScrap: false),
         FeedModel(itemImg: UIImage(named: "feed4") ?? UIImage(), name: "샌드위치", price: "8,000원", isScrap: false),
-        FeedModel(itemImg: UIImage(named: "feed5") ?? UIImage(), name: "명품 핸수", price: "250,000원", isScrap: false)
+        FeedModel(itemImg: UIImage(named: "feed5") ?? UIImage(), name: "명품 핸수", price: "250,000원", isScrap: false),
+        FeedModel(itemImg: UIImage(named: "feed1") ?? UIImage(), name: "아이패드 PRO", price: "1,400,000원", isScrap: false),
+        FeedModel(itemImg: UIImage(named: "feed2") ?? UIImage(), name: "airPod PRO 2", price: "330,000원", isScrap: false),
+        FeedModel(itemImg: UIImage(named: "feed3") ?? UIImage(), name: "마우스", price: "100,000원", isScrap: false),
+        FeedModel(itemImg: UIImage(named: "feed4") ?? UIImage(), name: "아 배고파", price: "8,000원", isScrap: false)
     ]
 }
